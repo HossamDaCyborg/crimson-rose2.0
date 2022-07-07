aac53e89ff8c44832b089d64c6dae5d33ab1aad9
HossamDaCyborg <hossamhamdy1482002@gmail.com>
2022-07-06 18:08:09 -0700

    much done

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,25 @@ module.exports = {
       xxl:"1440px",
     },
     
-    extend: {},
+    extend: {
+      colors:{
+        crimson : 'hsla(357, 82%, 39%, 1)',
+        crimsonHover:'hsla(357, 82%, 39%,0.6 )',
+        rreed:'hsla(357, 82%, 39%, .3)',
+        darkGray:'hsla(0, 0%, 38%, 1)',
+        lightGray:'hsla(0, 0%, 96%, 1)',
+        middleGray:'hsla(240, 2%, 64%, 1)',
+      },
+      fontSize:{
+        head:['50px','34px'],
+        textBox:['15px','18px'],
+        medButton:['15px','17.58px'],
+  
+      },
+      borderRadius:{
+        meddButton:'10px',
+      }
+  },
   },
   plugins: [require('daisyui')],
 }
